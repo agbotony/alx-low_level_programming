26b175f8bd7514747b46761309c72ded7af999de
agbotony <iamodoanthony@gmail.com>
2022-12-10 00:18:08 -0800

    eers

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#incude < stdio.h >
+#include < stdio.h >
 /**
  * main - Entry
  *
@@ -6,6 +6,6 @@
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
