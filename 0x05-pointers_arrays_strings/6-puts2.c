05c0ff9866b8e287ee6ef38e8bd7714f61665e5e
agbotony <iamodoanthony@gmail.com>
2022-12-19 15:01:49 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,6 @@
 
 /**
  * puts2 - prints every other character of a string
- *
  * @str: char to check
  *
  * Return: 0 is success
@@ -12,7 +11,7 @@ void puts2(char *str)
 	int string;
 
 	for (string = 0; str[string] != '\0'; string++)
-	if (string % 2 == 0)
-				_putchar(str[string];
+		if (string % 2 == 0)
+			_putchar(str[string]);
 	_putchar('\n');
 }
