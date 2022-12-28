bf900a6a01bffcd3276c49d29423e04f17d34ed2
agbotony <iamodoanthony@gmail.com>
2022-12-28 06:36:01 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
-*print_numbers - print 0123456789
-* owned by Bwave ICT
-*
-* *Return: void
+ * print_numbers - print 0123456789
+ * Owned by Tony
+ * Return: void
 */
-
 void print_numbers(void)
 {
+	char c;
 
-char c;
-for (c = '0'; c <= '9'; c++)
-
-{
-_putchar(c);
-_putchar('\n');
-
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+		_putchar('\n');
+	}
 }
